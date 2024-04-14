1a5188ba65727e6157f524ff6a59bd4546760d12
Hery Ruiz <heryrdev@gmail.com>
2024-04-14 14:55:39 -0400

    added nav and created footer

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Nav from './Nav.jsx'
+import Footer from "./Footer.jsx";
 function Layout() {
   return (
     <>
+      <Nav />
       <Outlet />
+      <Footer />
     </>
   );
 }
---
+++ src/pages/layout/Nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./images/logo.png";
+import "./styles/Nav.css";
+function Nav() {
+  return (
+    <nav>
+        <div className="nav__container">
+          <div className="nav__left">
+            <img src={logo} alt="FileStore Logo" />
+            <p className="nav__title">FileStore</p>
+          </div>
+          <div className="nav__right">
+            <button className="nav__signin">Sign In</button>
+          </div>
+        </div>
+    </nav>
+  );
+}
+
+export default Nav;
